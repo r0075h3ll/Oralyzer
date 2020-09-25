104232eee7bfeca67a8e7265242a723f49ccce1c
Hardik Nanda <hnanda21@gmail.com>
2020-09-25 09:17:50 +0530

    Update - Using a separate module for making requests awa for applying colors

---
+++ core/others.py
@@ -0,0 +1,28 @@
+good = "[\033[1m\033[92m•\033[00m\033[00m]"
+bad = "[\033[1m\033[91m•\033[00m\033[00m]"
+info = "[\033[1m\033[93m•\033[00m\033[00m]"
+
+import requests,random,json
+
+user = ['Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36 OPR/43.0.2442.991',
+'Mozilla/5.0 (Linux; U; Android 4.2.2; en-us; A1-810 Build/JDQ39) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Safari/534.30',
+'Mozilla/5.0 (Windows NT 5.1; rv:52.0) Gecko/20100101 Firefox/52.0',
+'Mozilla/5.0 (PLAYSTATION 3 4.81) AppleWebKit/531.22.8 (KHTML, like Gecko)',
+'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36 OPR/48.0.2685.52',
+'Mozilla/5.0 (SMART-TV; X11; Linux armv7l) AppleWebKit/537.42 (KHTML, like Gecko) Chromium/25.0.1349.2 Chrome/25.0.1349.2 Safari/537.42',
+'Mozilla/5.0 (Windows NT 6.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/601.2.7 (KHTML, like Gecko)',
+'Mozilla/5.0 (PlayStation 4 5.01) AppleWebKit/601.2 (KHTML, like Gecko)']
+
+
+header = {'User-Agent': random.choice(user)}
+with open('core/config.json', 'r') as file:
+    proxies = json.load(file)['proxies'][0]
+
+def requester(url,proxy):
+    if proxy:
+        webpage = requests.get(url, allow_redirects=False, headers=header, proxies=proxies, verify=False ,timeout=30)
+    else:
+        webpage = requests.get(url, allow_redirects=False, headers=header, timeout=10, verify=False)
+
+    return webpage
